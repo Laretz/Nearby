2f065f2801c47acf209c765c5fa10f5dce6bd9cb
Renato Ramon <renatophoenix123@gmail.com>
2024-12-15 12:43:48 -0300

    feat: criar componente Button com suporte para título e integrar ao Index
    - Adiciona o componente `Button` para reutilização de botões no projeto, com estilização personalizada.
    - Implementa o subcomponente `Button.Title` para adicionar títulos estilizados dentro do botão.
    - Define estilos para o botão, incluindo altura, cor, bordas arredondadas, alinhamento e espaçamento.
    - Integra o `Button` no arquivo `Index`, exibindo o botão com o texto "Começar" ao lado dos componentes `Welcome` e `Steps`.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,16 @@
 import { Welcome } from "@/components/welcome"
 import {View, Text} from "react-native"
 import { Steps } from "@/components/steps"
+import { Button } from "@/components/button"
 export default function Index(){
     return(
         <View style={{
             flex:1,padding: 40, gap: 40}}>
                <Welcome />
                <Steps />
+               <Button>
+                    <Button.Title>Começar</Button.Title>
+               </Button>
         </View>
     )
 }
