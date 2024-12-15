942a8b59a06767d96d43704e2d3fb68a896363cf
Renato Ramon <renatophoenix123@gmail.com>
2024-12-15 15:41:48 -0300

    feat(home): implementar tela inicial e navegação básica
    - Criado componente `Home` com:
      - Carregamento dinâmico de categorias e mercados via API.
      - Exibição de categorias e mercados com base na categoria selecionada.
    - Atualizado `index` para adicionar botão que navega para a tela `Home`.
    - Configurado `layout` com fontes personalizadas usando `@expo-google-fonts/rubik` e suporte a gestos com `react-native-gesture-handler`.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import {Stack} from "expo-router"
 import {colors} from "@/styles/theme"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 
 import {useFonts, Rubik_600SemiBold, Rubik_400Regular, Rubik_500Medium, Rubik_700Bold } from "@expo-google-fonts/rubik"
 import { Loading } from "@/components/loading"
@@ -17,11 +18,13 @@ export default function Layout(){
             return <Loading/>
         }
     return ( 
+        <GestureHandlerRootView style={{flex:1}}>
         <Stack 
             screenOptions={{
                 headerShown: false, 
                 contentStyle: {backgroundColor: colors.gray[100]},
             }}
             />
+        </GestureHandlerRootView>
     )
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,13 +2,14 @@ import { Welcome } from "@/components/welcome"
 import {View, Text} from "react-native"
 import { Steps } from "@/components/steps"
 import { Button } from "@/components/button"
+import { router } from "expo-router"
 export default function Index(){
     return(
         <View style={{
             flex:1,padding: 40, gap: 40}}>
                <Welcome />
                <Steps />
-               <Button>
+               <Button onPress={() => router.navigate("/home")}>
                     <Button.Title>Começar</Button.Title>
                </Button>
         </View>
